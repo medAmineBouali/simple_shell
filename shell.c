11ea49c345ccbfa42d9407102986d4309f4fa8c6
amine1 <medaminebouali1@gmail.com>
2023-05-19 23:00:11 +0000

    task

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+int calc_args(char *buffer);
 /**
  * main - Entry point.
  * 
@@ -12,12 +13,14 @@
 
 void main(void)
 {
+	int i;
 	char *buffer;
 	size_t bufsize = 32;
 	struct stat status;
-	char delim[] = "\n";
-	char *const argv[] = {NULL};
+	char delim1[] = "\n", delim2[] = " ";
+	char **argvs;
 	char *const envp[] = {NULL};
+	char *argv;
 
 	buffer = (char *)malloc(bufsize * sizeof(char));
 	if( buffer == NULL)
@@ -28,10 +31,17 @@ void main(void)
 	printf("#cisfun$ ");
 	if (getline(&buffer,&bufsize,stdin) == -1)
 		exit(0);
-	strtok(buffer,delim);
+	strtok(buffer, delim1);
+	argvs = (char **)malloc((calc_args(buffer) + 2)* sizeof(char *));
+	argv = strtok(buffer, delim2);
 	if (stat(buffer, &status) == 0)
 	{
-		if(execve(buffer, argv, envp) == -1)
+		for (i = 0; argv; i++)
+		{
+			*(argvs + i) = argv;
+			argv = strtok(NULL, delim2);
+		}
+		if(execve(buffer, argvs, envp) == -1)
 		{
 			perror("Could not execve");
 		} else
@@ -41,4 +51,21 @@ void main(void)
 			perror("./shell: No such file or directory");
 			main();
 		}
+}
+
+/**
+ * calc_args - calculates the number of arguments enterd.
+ * @buffer: the command line entered. \
+ *
+ * Return: number or arguments.
+ */
+int calc_args(char *buffer)
+{
+	int i, n = 0;
+	for (i = 0; buffer[i]; i++)
+	{
+		if(buffer[i] == '-')
+			n++;
+	}
+	return (n);
 }
