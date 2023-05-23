cff5fb4c453dde3dfc2242956601863990297bc4
amine1 <medaminebouali1@gmail.com>
2023-05-23 21:39:10 +0000

    task

--- tests.c
+++ tests.c
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-#include <wait.h>
 int calc_args(char *buffer);
 /**
  * main - Entry point.
@@ -14,14 +13,13 @@ int calc_args(char *buffer);
 
 int main(void)
 {
-	int i, ii;
-	pid_t pid;
+	int i;
 	char *buffer;
 	struct stat status;
 	size_t bufsize = 32;
 	char delim1[] = "\n", delim2[] = " ";
 	char **argvs;
-	char *envp[] = {"HOME=/root", "PATH=/bin:/sbin", NULL};
+	char *envp[] = {"PATH=/bin", NULL};
 	char *argv;
 
 	buffer = (char *)malloc(bufsize * sizeof(char));
@@ -34,48 +32,25 @@ int main(void)
 	if (getline(&buffer,&bufsize,stdin) == -1)
 	{
 		printf("\n");
-		exit(EXIT_FAILURE);
+		exit(0);
 	}
 	strtok(buffer, delim1);
-	ii = (calc_args(buffer) + 2);
-	argvs = (char **)malloc(ii * sizeof(char *));
+	argvs = (char **)malloc((calc_args(buffer) + 2)* sizeof(char *));
 	argv = strtok(buffer, delim2);
-	if (stat(buffer, &status) != 0)
-	{
-		perror("./shell1");
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork failed");
-		return (1);
-	}
-	if (pid != 0)
-	{
-		wait(&i);
-		for (i = 0; i < ii; i++)
-		{
-			free(argvs[i]);
-		}
-		free(argvs);
-		free(buffer);
-		main();
-	}
-	else
+	if (stat(buffer, &status) == 0)
 	{
 		for (i = 0; argv; i++)
 		{
 			*(argvs + i) = argv;
 			argv = strtok(NULL, delim2);
 		}
-		if(execve(buffer, argvs, envp) == -1)
+		main();
+	} else
 		{
 			perror("./shell");
+			main();
 		}
-		main();
-	}
-
-	return (0);
+	return (1);
 }
 
 /**
